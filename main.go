350436b35b1ba004194011227d841ece072d18bf
Barry <zjg555543@163.com>
2024-10-10 14:38:44 +0800

    Persit test for long time (#1)
    * update

--- main.go
+++ main.go
@@ -3,12 +3,12 @@ package main
 import (
 	"context"
 	"fmt"
-	"sync"
 	"time"
 
 	"github.com/tonkeeper/tongo"
 	"github.com/tonkeeper/tongo/config"
 	"github.com/tonkeeper/tongo/liteapi"
+	"github.com/tonkeeper/tongo/ton"
 )
 
 func main() {
@@ -20,30 +20,38 @@ func main() {
 		fmt.Printf("Unable to create tongo client: %v", err)
 	}
 	// accountId := tongo.MustParseAccountID("0:E2D41ED396A9F1BA03839D63C5650FAFC6FCFB574FD03F2E67D6555B61A3ACD9")
-	account := tongo.MustParseAddress("0:45CF4642228CB12F514BB3A79C389A7BAA7A10A92E8ECF4B915D246B7C2708CB")
+	accountList := []ton.Address{tongo.MustParseAddress("0:A560DC2F757589A7152835573D23117910207DDEDEFA717DBC82D8CF932E31B2"),
+		tongo.MustParseAddress("-1:CFB0B11350E4052FB60ED14CBF2FEDB265D7AC9EEB4C6BE0EEEE60AA2CE5DB3E"),
+		tongo.MustParseAddress("-1:5555555555555555555555555555555555555555555555555555555555555555"),
+		tongo.MustParseAddress("-1:0000000000000000000000000000000000000000000000000000000000000000"),
+		tongo.MustParseAddress("0:A560DC2F757589A7152835573D23117910207DDEDEFA717DBC82D8CF932E31B2")}
+	testTime := 10000 * time.Second
 
-	var wg sync.WaitGroup
-	req_count := 1000
-	for i := 0; i < 1000/req_count; i++ {
-		for j := 0; j < req_count; j++ {
-			call := func() {
-				defer wg.Done()
-
+	req_count := 10
+	total_req_count := 0
+	for j := 0; j < req_count; j++ {
+		call := func() {
+			count := 0
+			totalElapsed := time.Duration(0)
+			for {
+				count++
+				total_req_count++
 				start := time.Now()
-				_, err := tongoClient.GetAccountState(context.Background(), account.ID)
-				elapsed := time.Now().Sub(start)
-				fmt.Printf("GetAccountState cost: %s\n", elapsed)
+				_, err := tongoClient.GetAccountState(context.Background(), accountList[j%len(accountList)].ID)
 				if err != nil {
 					fmt.Printf("Get account state error: %v", err)
+					return
+				}
+				totalElapsed += time.Since(start)
+				if count%100 == 0 {
+					fmt.Printf("%v, GetAccountState avg cost: %v ms，totalElapsed:%v, count:%v, total_req_count:%v \n", j, int(totalElapsed.Milliseconds())/count, totalElapsed, count, total_req_count)
+					count = 0
+					totalElapsed = 0
 				}
 			}
-
-			wg.Add(1)
-			go call()
 		}
 
-		wg.Wait()
-		fmt.Println("------------")
-		// fmt.Printf("Account status: %v\nBalance: %v\n", state.Account.Status(), state.Account.Account.Storage.Balance.Grams)
+		go call()
 	}
+	time.Sleep(testTime)
 }
